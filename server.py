68390715d3f429c8b539efdf92b14c234eeb6a60
VA <dclass096@gmail.com>
2023-10-28 12:45:20 +0500

    pipenv:  -Pipfile  -Pipfile.lock .py files:  -server.py .html files:  -index.html

---
+++ server.py
@@ -0,0 +1,22 @@
+# import flask_login
+from flask import Flask, request, render_template, make_response, session, redirect
+# from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+# from flask_wtf import FlaskForm
+from jinja2 import FileSystemLoader, Environment
+
+
+app = Flask(__name__, template_folder="templates")
+app.config["SECRET_KEY"] = 'secret_key'
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+file_loader = FileSystemLoader("templates")
+env = Environment(loader=file_loader)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
