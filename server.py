77b8810f4a464f9111aa86213ead5d6867092d2f
VA <dclass096@gmail.com>
2023-10-29 10:26:13 +0500

    Часть тестов и часть связывания html и css

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template, make_response, session, redirect
+from flask import Flask, request, render_template, make_response, session, redirect, url_for
 from data import db_session
 from data.user import User
 
 from forms.loginform import LoginForm
+from forms.testform import TestForm
 from forms.user import RegisterForm
-
 
 from init import *
 from functions import *
@@ -35,8 +35,35 @@ def reviews():
     return render_template('reviews.html', title='Отзывы')
 
 
+@app.route('/portal/<string:portal>/<int:test>/<int:quest>', methods=['GET'])
+def test(portal, test, quest):
+    form = TestForm
+    db_sess = db_session.create_session()
+
+    return render_template('tests.html', title='Отзывы', portal=portal, test=test, quest=quest, form=form)
+
+
+@app.route('/back', methods=['GET', 'POST'])
+def q_back_page():
+    adr_req = [i for i in request.referrer.split("/")][-4:]
+    adr_req[-1] = str(int(adr_req[-1]) - 1)
+    back = "/" + "/".join(adr_req)
+    # print(back)
+    # print(request.method)
+    # redirect(location=back)
+    return redirect(location=back)
+
+
+@app.route('/forward', methods=['GET', 'POST'])
+def q_forward_page():
+    adr_req = [i for i in request.referrer.split("/")][-4:]
+    adr_req[-1] = str(int(adr_req[-1]) + 1)
+    forward = "/" + "/".join(adr_req)
+    # print(forward)
+    # print(request.method)
+    return redirect(location=forward)
 
 
 if __name__ == "__main__":
     db_session.global_init("db/db.db")
-    app.run(port=8080, host="127.0.0.1")
+    app.run(port=8080, host="127.0.0.1", debug=True)
